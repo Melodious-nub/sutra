857bd25aa9529c294e79351dc186152d1303024e
Melodious-nub <shawon.taluckder1@gmail.com>
2023-12-05 17:55:35 +0600

    updated with angular 17 and sutra

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { HttpClientInterceptor } from './http-client.interceptor';
 
 @NgModule({
@@ -28,6 +28,7 @@ import { HttpClientInterceptor } from './http-client.interceptor';
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration(),
